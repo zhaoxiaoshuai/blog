1b894ff962a46537d2e1ffb425024f41da3a6cfe
zhaoxiaoshuai <460779420@qq.com>
2017-08-07 13:52:16 +0800

    ssh测试

--- Blog/Admin/Controller/AdminController.class.php
+++ Blog/Admin/Controller/AdminController.class.php
@@ -3,7 +3,6 @@ namespace Admin\Controller;
 use Think\Controller;
 class AdminController extends Controller {
     public function add(){
-        $a = 1;
         $this -> display();
     }
     public function insert(){
