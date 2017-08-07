f06a801d2c760be8cdbf5cd089ae2e6b1e42e69f
从没开始 <506907958@qq.com>
2017-08-07 22:39:08 +0800

    紧急上线

--- Blog/Admin/Conf/config.php
+++ Blog/Admin/Conf/config.php
@@ -5,6 +5,7 @@ return array(
         '用户管理' => array(
             '用户列表' => '/admin/member/index',
             '添加用户' => '/admin/member/add',
+            '已删除用户' => '/admin/member/deleted',
         ),
         '文章管理' => array(
             '文章列表' => '/admin/article/index',
@@ -18,5 +19,22 @@ return array(
             '导航列表' => '/admin/article/index',
             '添加导航' => '/admin/article/add',
         ),
+    ),
+    'SEX' => array(
+        'm' => '男',
+        'w' => '女',
+        'x' => '保密'
+    ),
+    'STATUS' => array(
+        1 => '未激活',
+        '正常',
+        '停用',
+        '已删除'
+    ),
+    'STATUS_ICON' => array(
+        1 => 'label-warm',
+        'label-success',
+        'label-warm',
+        'label-danger'
     ),
 );
