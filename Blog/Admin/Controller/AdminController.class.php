6ea712480c0aee6eda3d49682cf306076a1d5d5a
zhaoxiaoshuai <460779420@qq.com>
2017-08-07 13:37:14 +0800

    测试

--- Blog/Admin/Controller/AdminController.class.php
+++ Blog/Admin/Controller/AdminController.class.php
@@ -3,6 +3,7 @@ namespace Admin\Controller;
 use Think\Controller;
 class AdminController extends Controller {
     public function add(){
+        $a = 1;
         $this -> display();
     }
     public function insert(){
