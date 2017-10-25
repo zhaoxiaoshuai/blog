f1030e27865c1064c7f7cabbaa2d3f9b52bb2a2f
root <root@localhost.localdomain>
2017-10-25 14:59:35 +0800

    上一次，没怎么看就提交，导致很多垃圾文件。这次我把开发环境换成 centos 7.3 64bit php7.1.20 mysql5.7.20

--- Blog/Common/Conf/config.php
+++ Blog/Common/Conf/config.php
@@ -10,7 +10,7 @@ return array(
     'DB_HOST'               =>  'localhost', // 服务器地址
     'DB_NAME'               =>  'blog',          // 数据库名
     'DB_USER'               =>  'xyj2156',      // 用户名
-    'DB_PWD'                =>  '000021560000',          // 密码
+    'DB_PWD'                =>  '0002156000',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'jief_',    // 数据库表前缀
     'DB_PARAMS'             =>  array(), // 数据库连接参数
